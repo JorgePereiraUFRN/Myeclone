fa4fbf25c1d0c2de3a0c910ff1e273c2ca6f5987
jorge <jorgepereirasb@gmail.com>
2014-05-21 22:52:47 -0300

    Renomeação das variaveis de algumas interfaces graficas

--- Myeclone/src/br/ufrn/Myeclone/GUI/Atividades.java
+++ Myeclone/src/br/ufrn/Myeclone/GUI/Atividades.java
@@ -18,7 +18,14 @@ public class Atividades extends javax.swing.JFrame {
     public Atividades() {
         initComponents();
     }
+    
+    private void consultar(String text){
+        
+    }
 
+    private void novo(){
+        
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -29,11 +36,11 @@ public class Atividades extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        jTpesquisar = new javax.swing.JTextField();
+        jBpesquisar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jButton2 = new javax.swing.JButton();
+        jTableAtividades = new javax.swing.JTable();
+        jBnovo = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
@@ -41,7 +48,12 @@ public class Atividades extends javax.swing.JFrame {
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Atividades"));
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/ufrn/Myeclone/icones/pesquisar.png"))); // NOI18N
+        jBpesquisar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/ufrn/Myeclone/icones/pesquisar.png"))); // NOI18N
+        jBpesquisar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBpesquisarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -49,23 +61,23 @@ public class Atividades extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(6, 6, 6)
-                .addComponent(jTextField1)
+                .addComponent(jTpesquisar)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1)
+                .addComponent(jBpesquisar)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jButton1)
-                .addGap(0, 5, Short.MAX_VALUE))
+                .addComponent(jBpesquisar)
+                .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jTpesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jTableAtividades.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -76,10 +88,15 @@ public class Atividades extends javax.swing.JFrame {
                 "Atividade", "Data", "Hora", "Cumprido"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jTableAtividades);
 
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/ufrn/Myeclone/icones/relogio.png"))); // NOI18N
-        jButton2.setText("Novo");
+        jBnovo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/ufrn/Myeclone/icones/relogio.png"))); // NOI18N
+        jBnovo.setText("Novo");
+        jBnovo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBnovoActionPerformed(evt);
+            }
+        });
 
         jLabel1.setText("Propagandas");
 
@@ -98,7 +115,7 @@ public class Atividades extends javax.swing.JFrame {
                         .addGap(32, 32, 32)
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton2)))
+                        .addComponent(jBnovo)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -110,13 +127,21 @@ public class Atividades extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
+                    .addComponent(jBnovo)
                     .addComponent(jLabel1))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jBpesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBpesquisarActionPerformed
+        consultar(jTpesquisar.getText().trim());
+    }//GEN-LAST:event_jBpesquisarActionPerformed
+
+    private void jBnovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBnovoActionPerformed
+        novo();
+    }//GEN-LAST:event_jBnovoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -154,12 +179,12 @@ public class Atividades extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jBnovo;
+    private javax.swing.JButton jBpesquisar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTable jTableAtividades;
+    private javax.swing.JTextField jTpesquisar;
     // End of variables declaration//GEN-END:variables
 }
