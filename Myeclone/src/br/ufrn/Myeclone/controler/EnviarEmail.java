aadd91cb4751d01415d9b94d083978dbc889f3fb
João Luca Vinaud <leyenasd@gmail.com>
2014-06-04 19:13:31 -0300

    Update EnviarEmail.java
    Classe de enviar email parametrizada, bunitinha. Agora ei de usar a interface para chama-la

--- Myeclone/src/br/ufrn/Myeclone/controler/EnviarEmail.java
+++ Myeclone/src/br/ufrn/Myeclone/controler/EnviarEmail.java
@@ -11,37 +11,65 @@ import javax.mail.internet.MimeMessage;
  
 public class EnviarEmail {
 	public static void main(String[] args) {
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
- 
-		Session session = Session.getDefaultInstance(props,
-			new javax.mail.Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication("leyenasd@gmail.com","");
-				}
-			});
- 
-		try {
- 
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("leyenasd@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse("leyenasd@gmail.com"));
-			message.setSubject("Testing Subject");
-			message.setText("Dear Mail Crawler," +
-					"\n\n No spam to my email, please!");
- 
-			Transport.send(message);
- 
-			System.out.println("Done");
- 
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
+            // main para teste de funcionamento
+		enviarEmail("login", "senha", "destinatário", "Assunto", "Corpo mensagem");
 	}
+
+    /**
+     *
+     * @param login
+     * @param senha
+     * @param destino
+     * @param assunto
+     * @param mensagem
+     * @return
+     */
+    public static String enviarEmail(final String login, final String senha, String destino, String assunto, String mensagem)
+        {
+          
+            Properties p = new Properties();
+            p.put("mail.smtp.host", "smtp.gmail.com");
+            p.put("mail.smtp.socketFactory.port", "465");
+            p.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+            p.put("mail.smtp.auth", "true");
+            p.put("mail.smtp.port", "465");
+ 
+            Session s = getSession(p, login, senha);
+ 
+            try {
+                    sendMessage(s, login, destino, assunto, mensagem);
+ 
+                    System.out.println("Enviado");
+                        
+                    return "Mensagem enviada com sucesso para" + destino ;
+ 
+		} 
+            
+            catch (MessagingException e)
+                 {
+                     throw new RuntimeException(e);                  
+                 }
+            
+        }
+
+    private static void sendMessage(Session s, final String login, String destino, String assunto, String mensagem) throws MessagingException {
+        Message m = new MimeMessage(s);
+        m.setFrom(new InternetAddress(login));
+        m.setRecipients(Message.RecipientType.TO,InternetAddress.parse(destino));
+        m.setSubject(assunto);
+        m.setText(mensagem);
+        Transport.send(m);
+    }
+
+    private static Session getSession(Properties p, final String login, final String senha) {
+        Session s = Session.getDefaultInstance(p,new javax.mail.Authenticator()
+        {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication()
+            {
+                return new PasswordAuthentication(login,senha);
+            }
+        });
+        return s;
+    }
 }
